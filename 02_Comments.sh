7cf8433b0446e3d02a4f23987bec39540f18b599
raheel911 <raheeel911@gmail.com>
2026-03-18 21:17:21 +0500

    Scripts on Bash

---
+++ 02_Comments.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+echo "Checking Comments"
+
+#This is Single line Comment
+
+
+
+<<comments
+This 
+is 
+Multiline
+Comments
+
+comments
---
+++ 03_variable.sh
@@ -0,0 +1,7 @@
+#!/bon/bash
+
+a=10
+name="raheel"
+age=20
+echo "My name is $name and age is $age"
+
